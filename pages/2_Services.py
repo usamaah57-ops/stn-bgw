b5f7531243039844598839b8a19c02a09cb286a8
usamaah57-ops <usamaah57@gmail.com>
2026-04-26 20:58:15 +0300

    Update 2_Services.py

--- pages/2_Services.py
+++ pages/2_Services.py
@@ -18,7 +18,7 @@ init_db()
 page_bg_img = f"""
 <style>
 [data-testid="stAppViewContainer"] {{
-background-image: url("assets/egyptair_bg.png");
+background-image: url("assets/egyptair_bg.jpg");
 background-size: cover;
 background-repeat: no-repeat;
 background-attachment: fixed;
