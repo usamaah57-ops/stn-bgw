010c3c46048f9571a305b2ba078b208bb4174aad
usamaah57-ops <usamaah57@gmail.com>
2026-04-26 19:24:39 +0300

    Update database.py

--- modules/database.py
+++ modules/database.py
@@ -38,29 +38,4 @@ def save_service(flight, reg, date, service, time, mode):
                       (time, flight, reg, date, service))
         elif mode == "end":
             c.execute("UPDATE services SET end=? WHERE flight=? AND reg=? AND date=? AND service=?",
-                      (time, flight, reg, date, service))
-    else:
-        if mode == "start":
-            c.execute("INSERT INTO services (flight, reg, date, service, start, end) VALUES (?, ?, ?, ?, ?, ?)",
-                      (flight, reg, date, service, time, None))
-        elif mode == "end":
-            c.execute("INSERT INTO services (flight, reg, date, service, start, end) VALUES (?, ?, ?, ?, ?, ?)",
-                      (flight, reg, date, service, None, time))
-
-    conn.commit()
-    conn.close()
-
-# -----------------------------------
-# Load services
-# -----------------------------------
-def load_services(flight, date):
-    conn = sqlite3.connect("services.db")
-    c = conn.cursor()
-    c.execute("SELECT service, start, end FROM services WHERE flight=? AND date=?", (flight, date))
-    rows = c.fetchall()
-    conn.close()
-
-    data = {}
-    for service, start, end in rows:
-        data[service] = {"start": start, "end": end}
-    return data
+                      (time, flight, reg
