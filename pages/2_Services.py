39991004d4ed92c0bfb33f124f2fdbd9ea66f326
usamaah57-ops <usamaah57@gmail.com>
2026-04-26 06:05:14 +0300

    Update 2_Services.py

--- pages/2_Services.py
+++ pages/2_Services.py
@@ -2,6 +2,12 @@ import streamlit as st
 import os
 import datetime
 import sys
+# ------------------------------
+# FIX IMPORT ERROR ON STREAMLIT CLOUD
+# ------------------------------
+BASE_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
+if BASE_DIR not in sys.path:
+    sys.path.insert(0, BASE_DIR)
 
 # -----------------------------
 # FIX IMPORT ERROR ON STREAMLIT CLOUD
